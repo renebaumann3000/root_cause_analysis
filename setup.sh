591851bc19b1b9c5bed79f43fdfcca3efb7c5052
Rene <rene.baumann84@outlook.com>
2023-12-31 22:44:42 +0100

    test script for deployment

--- setup.sh
+++ setup.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
+
+# Custom PyTorch CPU-only installation
+pip install --index-url https://download.pytorch.org/whl/cpu torch==2.1.1
+
+# Install other requirements
+pip install -r requirements.txt
+
+# Setup Streamlit configuration
 mkdir -p ~/.streamlit/
+
 echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
